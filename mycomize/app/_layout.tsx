80e82ba089ca2664b724e5a6b63fa637f03e0e12
Connor Davis <connojdavis@gmail.com>
2025-05-03 20:45:28 -0600

    update frontend

---
+++ mycomize/app/_layout.tsx
@@ -0,0 +1,12 @@
+import '~/global.css';
+import { Stack } from 'expo-router';
+
+export default function RootLayout() {
+  return (
+    <>
+      <Stack>
+        <Stack.Screen name="(protected)" options={{ headerShown: false }} />
+      </Stack>
+    </>
+  );
+}
